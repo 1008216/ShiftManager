47eb0d0faf2548eff920f874fd8918ea320ea33a
東元一晃 <tougenkazuteru@tougenkazuterunoMacBook-Pro.local>
2021-02-06 12:45:49 +0900

    メニュー画面の実装

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import { app } from "../base";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 function Home(props) {
+  const classes = useStyles();
   return (
-    <div>
-      <h2>Home Page</h2>
-      // ユーザーをログアウトさせる
-      <button onClick={() => app.auth().signOut()}>Sign out</button>
+    <div className={classes.root}>
+     
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="月間シフト" />
+        </ListItem>
+        <Divider />
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="管理者予定登録" />
+        </ListItemLink>
+        <Divider />
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="個人予定登録" />
+        </ListItemLink>
+        <Divider />
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="ユーザー管理画面" />
+        </ListItemLink>
+        <Divider />
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="パスワード登録・変更" />
+        </ListItemLink>
+      </List>
     </div>
   );
 }
